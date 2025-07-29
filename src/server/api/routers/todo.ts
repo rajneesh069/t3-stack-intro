b36bb37324360d5368543858a417e36df9ee1de0
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2025-07-29 16:41:47 +0530

    add todo model, and CRUD to it

---
+++ src/server/api/routers/todo.ts
@@ -0,0 +1,87 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+
+export const todoRouter = createTRPCRouter({
+  addTodo: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        done: z.boolean(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const userId = ctx.session.user.id;
+      return await ctx.db.todo.create({
+        data: { ...input, userId },
+      });
+    }),
+
+  getTodo: protectedProcedure
+    .input(z.object({ userId: z.string().uuid() }))
+    .query(async ({ input, ctx }) => {
+      return await ctx.db.todo.findMany({
+        where: {
+          userId: input.userId,
+        },
+      });
+    }),
+
+  updateTodo: protectedProcedure
+    .input(
+      z.object({
+        todoId: z.string().uuid(),
+        todo: z.object({
+          title: z.string(),
+          description: z.string(),
+          done: z.boolean(),
+        }),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const userId = ctx.session.user.id;
+      const result = await ctx.db.todo.updateManyAndReturn({
+        where: {
+          id: input.todoId,
+          userId,
+        },
+        data: input.todo,
+      });
+
+      if (result.length === 0) {
+        throw new TRPCError({
+          message: "Todo not found!",
+          code: "BAD_REQUEST",
+        });
+      }
+
+      return { data: result[0] };
+    }),
+
+  deleteTodo: protectedProcedure
+    .input(
+      z.object({
+        todoId: z.string().uuid(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const userId = ctx.session.user.id;
+
+      const { count } = await ctx.db.todo.deleteMany({
+        where: {
+          id: input.todoId,
+          userId,
+        },
+      });
+
+      if (count === 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Todo not found!!",
+        });
+      }
+
+      return { success: true };
+    }),
+});
